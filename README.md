4f01a8d93ec83548b8e4ed543fd9c842b1261d15
bepnos_ <bepnos@proton.me>
2023-08-02 23:46:35 +0200

    Initial Commit
    Create README.md
    
    update
    
    update
    
    new neovim config
    
    new neovim config
    
    trying to fix html lsp
    
    small neovim tweaks lsp html fixed maar geen autocomplete tags
    
    file browser through telescope
    
    html autotags
    
    new neovim config with lunarvim
    
    Added several plugins
    
    almost IDE reade
    
    added discord rich presence
    
    changed to zap
    
    added a lot of stuff
    
    cleaned up config
    
    cleaned up some more

---
+++ README.md
@@ -0,0 +1,2 @@
+# dotfiles
+My dotfiles that I use across my devices, "Whats with the weird names?" I use the package [chezmoi](https://www.chezmoi.io/) to sync my dotfiles which handles all the naming and interaction between my machines and repo.
---
+++ private_dot_config/lvim/lua/bepnos/options.lua
@@ -0,0 +1,96 @@
+-- Settings configuration
+vim.api.nvim_set_option("clipboard","unnamedplus")
+vim.opt.relativenumber = true
+lvim.colorscheme = "gruvbox"
+lvim.transparent_window = true
+lvim.builtin.nvimtree.setup.sync_root_with_cwd = false
+lvim.builtin.terminal.open_mapping = "<c-t>"
+vim.opt.number = true
+-- List of plugins to load
+lvim.plugins = {
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  {
+  "ahmedkhalf/lsp-rooter.nvim",
+  event = "BufRead",
+  config = function()
+    require("lsp-rooter").setup()
+  end,
+  },
+  {
+  "Pocco81/auto-save.nvim",
+  config = function()
+    require("auto-save").setup()
+    end,
+  },
+  {
+    "0x100101/lab.nvim"
+  },
+  {
+  "ggandor/leap.nvim",
+  name = "leap",
+  config = function()
+    require("leap").add_default_mappings()
+  end,
+  },
+{"andweeb/presence.nvim"},
+{ "nvim-neotest/neotest"},
+{"nvim-neotest/neotest-python"},
+{"mfussenegger/nvim-dap-python"},
+{"stevearc/dressing.nvim"},
+  {"AckslD/swenv.nvim"},
+}
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
+})
+
+require("presence").setup({
+  log_level = "debug",
+})
+-- lab.nvim config
+require("lab").setup{
+  code_runner = {
+    enabled = true,
+  },
+  quick_date = {
+    enabled = true,
+  }
+}
+lvim.builtin.which_key.mappings["r"] = {
+  name = "Runners",
+ r = {"<cmd> Lab code run<CR>", "Run lab"},
+ s = {"<cmd> Lab code stop<CR>", "Stop lab"},
+}
+
+-- greeter custom logo
+lvim.builtin.alpha.dashboard.section.header.val = {
+    "                                    ██████                                    ",
+    "                                ████▒▒▒▒▒▒████                                ",
+    "                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ",
+    "                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ",
+    "                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ",
+    "                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ",
+    "                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ",
+    "                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ",
+    "                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ",
+    "                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ",
+    "                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ",
+    "                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ",
+    "                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ",
+    "                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ",
+    "                        ██      ██      ████      ████                        ",
+}
+lvim.builtin.alpha.dashboard.section.header.opts.hl = "HighlightGroupName"
