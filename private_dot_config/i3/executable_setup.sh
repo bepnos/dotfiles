c2106a78da13df47afd720825a01c975c923b276
bepnos_ <bepnos@proton.me>
2023-08-27 18:24:32 +0200

    arch desktop fully functional

--- private_dot_config/i3/executable_setup.sh
+++ private_dot_config/i3/executable_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(hostname)" = "bepnos-desktop" ]; then
+if [ "$(hostname)" = "desktop-arch" ]; then
   ln -sf ~/.config/i3/desktop-config.conf ~/.config/i3/machine-config
 else
   ln -sf ~/.config/i3/laptop-config.conf ~/.config/i3/machine-config
--- private_dot_config/nvim/lua/bepnos/set.lua
+++ private_dot_config/nvim/lua/bepnos/set.lua
@@ -1,4 +1,3 @@
-
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
